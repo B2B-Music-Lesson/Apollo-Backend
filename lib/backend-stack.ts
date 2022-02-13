fbd0b030a5b05c54280a18712ed8ed3e40fda152
Camy ngo <camyngo@byu.edu>
2022-02-12 21:22:02 -0700

    created dynamoDB tables

--- lib/backend-stack.ts
+++ lib/backend-stack.ts
@@ -1,19 +1,28 @@
-import { Duration, Stack, StackProps } from 'aws-cdk-lib';
-import * as sns from 'aws-cdk-lib/aws-sns';
-import * as subs from 'aws-cdk-lib/aws-sns-subscriptions';
-import * as sqs from 'aws-cdk-lib/aws-sqs';
+import { Stack, StackProps } from 'aws-cdk-lib';
+import * as dynamodb from 'aws-cdk-lib/aws-dynamodb';
 import { Construct } from 'constructs';
 
 export class BackendStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const queue = new sqs.Queue(this, 'BackendQueue', {
-      visibilityTimeout: Duration.seconds(300)
+    const userTable = new dynamodb.Table(this, 'User', {
+        partitionKey: { name: 'user_id', type: dynamodb.AttributeType.STRING }
     });
 
-    const topic = new sns.Topic(this, 'BackendTopic');
+    const user_cardSet = new dynamodb.Table(this, 'User_CardSet', {
+      partitionKey: { name: 'id', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'timestamp', type: dynamodb.AttributeType.NUMBER}
+    })
 
-    topic.addSubscription(new subs.SqsSubscription(queue));
+    const cardSetTable = new dynamodb.Table(this, 'CardSet', {
+      partitionKey: { name: 'cardset_id', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'created_by', type: dynamodb.AttributeType.STRING }
+    })
+
+    const cardTable = new dynamodb.Table(this, 'Card', {
+      partitionKey: { name: 'card_id', type: dynamodb.AttributeType.STRING },
+      sortKey: { name: 'belongs_to', type: dynamodb.AttributeType.STRING }
+    })
   }
 }
