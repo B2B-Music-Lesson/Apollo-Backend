2522689db6538b686c0518ef2f88af8d082837e7
Camy ngo <camyngo@byu.edu>
2022-02-26 18:56:53 -0700

    create get user lamda

--- lambda/getUserById.ts
+++ lambda/getUserById.ts
@@ -1,14 +1,36 @@
 import { APIGatewayProxyHandler } from 'aws-lambda'
+import * as AWS from 'aws-sdk'
 
-export const handler: APIGatewayProxyHandler = async function (event) {
-    return {
-        body: JSON.stringify({
-            user_id: 'bogey',
-            password: 'yomamma',
-            is_teacher: false,
-            card_sets: [],
-            sets_num: 0
-        }),
-        statusCode: 200
+
+const db = new AWS.DynamoDB.DocumentClient();
+const TABLE_NAME = 'BackendStack-User00B015A1-B1D5X81Q9YMT';
+
+export const handler: APIGatewayProxyHandler = async (event: any = {}): Promise<any> => {
+        const params = {
+            TableName: TABLE_NAME,
+            Item: {
+                'user_id': 'fgfg',
+                'is_teacher': false,
+                'password': 'dgdg'
+            }
+        };
+        
+        await db.get(params).promise();
+
+        const item = params.Item;
+
+        // Check if parameters are valid
+        if (!(item?.user_id && item?.password && item?.is_teacher)) {
+            return {
+                statusCode: 400,
+                headers: {},
+                body: '[InvalidRequest]',
+            };
+        } else {
+            return {
+                statusCode: 200,
+                body: 'success'
+            }
+            
+        }
     }
-}
