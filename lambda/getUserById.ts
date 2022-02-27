242b2f9ccedc0dd5618f564ae512c145587f6c92
Camy ngo <camyngo@byu.edu>
2022-02-26 23:26:30 -0700

    update getUser with password

--- lambda/getUserById.ts
+++ lambda/getUserById.ts
@@ -6,12 +6,14 @@ const db = new AWS.DynamoDB.DocumentClient();
 const TABLE_NAME = 'BackendStack-User00B015A1-B1D5X81Q9YMT';
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-        const id = event.queryStringParameters ? event.queryStringParameters.user_id: ''
+    const id = event.queryStringParameters ? event.queryStringParameters.user_id : ''
+    const password = event.queryStringParameters ? event.queryStringParameters.password: ''
         const params = {
             TableName: TABLE_NAME,
             
         Key: {
             'user_id': id,
+            'password': password
         }
     };
     var { Item } = await db.get(params).promise()
