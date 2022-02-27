1a716660ff1b4143b2f998a1ee7c35b56d96e3f9
Camy ngo <camyngo@byu.edu>
2022-02-27 03:51:15 -0700

    Revert "clean up"
    This reverts commit 22a0cb597be5587c89bb8b8c4b13af7d629e0b8f.

--- lambda/getUserById.ts
+++ lambda/getUserById.ts
@@ -6,9 +6,8 @@ const db = new AWS.DynamoDB.DocumentClient();
 const TABLE_NAME = 'BackendStack-User00B015A1-B1D5X81Q9YMT';
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-    const id = event.queryStringParameters ? event.queryStringParameters : ''
+    const id = event.queryStringParameters ? event.queryStringParameters.user_id : ''
     const password = event.queryStringParameters ? event.queryStringParameters.password: ''
-    
         const params = {
             TableName: TABLE_NAME,
             
