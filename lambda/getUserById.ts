decbe46016be1da361e72ec41b79f092487500b3
Camy ngo <camyngo@byu.edu>
2022-04-08 22:24:39 -0600

    get user test done

--- lambda/getUserById.ts
+++ lambda/getUserById.ts
@@ -1,25 +1,29 @@
-import { APIGatewayProxyHandler } from 'aws-lambda'
-import * as AWS from 'aws-sdk'
-AWS.config.update({region:'us-west-2'});
-const db = new AWS.DynamoDB.DocumentClient();
-console.log("table name", process.env.TABLE_NAME)
-export const handler: APIGatewayProxyHandler = async (event) => {
-    const id = event.queryStringParameters ? event.queryStringParameters.user_id : ''
-    const params = {
-        TableName: process.env.TABLE_NAME||'',
-        Key: {
-            'user_id': id,
-        }
-    };
-    var { Item } = await db.get(params).promise()
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { createDbConnection } from "../utils";
 
-    console.log(JSON.stringify(Item))
-    return {
-        statusCode: 200,
-        body: JSON.stringify(Item),
-        headers: {
-            'Access-Control-Allow-Origin': '*',
-            'Access-Control-Allow-Methods': '*'
-        }
-    }
-}
+// import * as AWS from 'aws-sdk'
+// AWS.config.update({region:'us-west-2'});
+// console.log("table name", process.env.TABLE_NAME)
+export const handler: APIGatewayProxyHandler = async (event) => {
+  const db = createDbConnection();
+  const id = event.queryStringParameters
+    ? event.queryStringParameters.user_id
+    : "";
+  const params = {
+    TableName: process.env.TABLE_NAME || "",
+    Key: {
+      user_id: id,
+    },
+  };
+  var { Item } = await db.get(params).promise();
+
+  console.log(JSON.stringify(Item));
+  return {
+    statusCode: 200,
+    body: JSON.stringify(Item),
+    headers: {
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "*",
+    },
+  };
+};
