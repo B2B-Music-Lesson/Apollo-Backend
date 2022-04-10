73439edfc0336f7746dc3c04cc872ca5c016381a
Camy ngo <camyngo@byu.edu>
2022-04-09 23:13:58 -0600

    update mock for login user Test

--- lambda/login.ts
+++ lambda/login.ts
@@ -1,48 +1,56 @@
-import { APIGatewayProxyHandler } from 'aws-lambda'
-import * as AWS from 'aws-sdk'
+import { APIGatewayProxyHandler } from "aws-lambda";
+import { createDbConnection } from "../utils/createDbConnection";
 
-const db = new AWS.DynamoDB.DocumentClient();
 const access = {
-    'Access-Control-Expose-Headers': 'Access-Control-Allow-Origin',
-    'Content-Type': 'application/json',
-    'Access-Control-Allow-Origin': '*',
-    'Access-Control-Allow-Methods': '*'
-}
+  "Access-Control-Expose-Headers": "Access-Control-Allow-Origin",
+  "Content-Type": "application/json",
+  "Access-Control-Allow-Origin": "*",
+  "Access-Control-Allow-Methods": "*",
+};
 
 export const handler: APIGatewayProxyHandler = async (event) => {
-    try {
-        const item = JSON.parse(event.body || '{}');
+  const db = createDbConnection();
+  try {
+    const item = JSON.parse(event.body || "{}");
 
-        console.log('body', event.body);
-        console.log("table name", process.env.TABLE_NAME);
+    console.log("body", event.body);
+    console.log("table name", process.env.TABLE_NAME);
 
-        const params = {
-            TableName: process.env.TABLE_NAME||'',
-            Key: {
-                'user_id': item.user_id,
-            }
-        };
-        var { Item } = await db.get(params).promise()
-
-        if (item?.password !== Item?.password) {
-            return {
-                statusCode: 403,
-                headers: access,
-                body: '[Unauthorized Access]',
-            };
-        }
-    
-        console.log(JSON.stringify(Item))
-        return {
-            statusCode: 200,
-            body: JSON.stringify(Item),
-            headers: access,
-        }
-    } catch(error) {
-        return {
-            statusCode: 500,
-            body: '[ServerError] '+  JSON.stringify(error),
-            headers: access
-        };
+    if (item.id && item.password) {
+      const params = {
+        TableName: process.env.TABLE_NAME || "",
+        Key: {
+          user_id: item.user_id,
+        },
+      };
+      var { Item } = await db.get(params).promise();
+    } else {
+      return {
+        statusCode: 400,
+        body: "missing use_id or password",
+        headers: access,
+      };
     }
-}
+
+    if (item?.password !== Item?.password) {
+      return {
+        statusCode: 403,
+        headers: access,
+        body: "[Unauthorized Access]",
+      };
+    }
+
+    console.log(JSON.stringify(Item));
+    return {
+      statusCode: 200,
+      body: JSON.stringify(Item),
+      headers: access,
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: "[ServerError] " + JSON.stringify(error),
+      headers: access,
+    };
+  }
+};
